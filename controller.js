f9f11a27126f6f8284acc5fc8859886e393182a7
August Conwell <august.conwell@gmail.com>
2017-06-16 23:04:29 -0400

    Added return

--- controller.js
+++ controller.js
@@ -3,24 +3,27 @@ var fs = require('fs');
 var path = require('path');
 var basename = path.basename(module.filename);
 
-var controllers = {}
+function setDirectory(directory) {
+    var files = {}
 
-//============== Collect all js files in controllers directory
-fs
-    .readdirSync(__dirname + '/../../controllers')
-    //============== Filter this file, hidden files, and non js files
-    .filter(function (file) {
-        return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-    })
-    //============== Require each of those into the controllers object
-    .forEach(function (file) {
-        var controller = file;
-        controllers[controller.slice(0, controller.length - 3)] = require('../../controllers/' + controller);
-    });
-
-module.exports = function (app) {
-    //============== Execute each controller function when this function is called
-    Object.keys(controllers).forEach(function (controller) {
-        controllers[controller](app);
-    });
+    //============== Collect all js files in controllers directory
+    fs
+        .readdirSync(__dirname + '/../../' + directory)
+        //============== Filter this file, hidden files, and non-js files
+        .filter(function (file) {
+            return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+        })
+        //============== Require each of those into the controllers object
+        .forEach(function (file) {
+            var controller = file;
+            controllers[controller.slice(0, controller.length - 3)] = require('../../' + directory + '/' + controller);
+        });
+    return function (app) {
+        //============== Execute each controller function when this function is called
+        Object.keys(controllers).forEach(function (controller) {
+            controllers[controller](app);
+        });
+    }
 }
+
+module.exports = setDirectory;
