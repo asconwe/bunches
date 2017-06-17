06fe9bf61f1266fbd62da66fad90d1e8294e63ae
August Conwell <august.conwell@gmail.com>
2017-06-16 23:14:59 -0400

    Rephrased collector

--- controller.js
+++ controller.js
@@ -6,22 +6,21 @@ var basename = path.basename(module.filename);
 function setDirectory(directory) {
     var files = {}
 
-    //============== Collect all js files in controllers directory
+    //============== Collect all js files in the specified directory
     fs
         .readdirSync(__dirname + '/../../' + directory)
         //============== Filter this file, hidden files, and non-js files
         .filter(function (file) {
             return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
         })
-        //============== Require each of those into the controllers object
+        //============== Require each of those into the files object
         .forEach(function (file) {
-            var controller = file;
-            controllers[controller.slice(0, controller.length - 3)] = require('../../' + directory + '/' + controller);
+            files[file.slice(0, file.length - 3)] = require('../../' + directory + '/' + file);
         });
     return function (app) {
-        //============== Execute each controller function when this function is called
-        Object.keys(controllers).forEach(function (controller) {
-            controllers[controller](app);
+        //============== Execute each exported function when this function is called
+        Object.keys(files).forEach(function (file) {
+            files[file](app);
         });
     }
 }
